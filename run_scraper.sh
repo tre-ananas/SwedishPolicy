ca64e0ea6f56da4aa8df4ca0e463c5f60860d0c0
Ryan Wolff <ryan.h.wolff@gmail.com>
2023-10-18 09:21:05 -0600

    Initial Push from Laptop

---
+++ run_scraper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p Logs
+
+start=1
+increment=3
+end=10
+
+while [[ $start -lt $end ]]; do
+    next=$((start + increment))
+    output_file="Logs/output_${start}_${next}"
+    
+    GovDocsOperationalV3.py $start $next > "${output_file}_out.txt" 2> "${output_file}_err.txt" &
+
+    start=$next
+done
